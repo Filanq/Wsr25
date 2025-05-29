675a91379a606363cd1ee1352933bd51b0b4fc7d
user <user@localhost.localdomain>
2025-05-29 11:06:55 +0300

    initial vue

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+
+</template>
+
+<script setup>
+    import { RouterLink, RouterView } from 'vue-router';
+    import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<style scoped>
+
+</style>
